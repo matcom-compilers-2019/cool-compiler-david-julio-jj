1ac437ad5023b6b4d0851f778cfdcc3d79ddcfe9
Joel David Hernandez Cruz <jdavidhc1710@gmail.com>
2019-04-14 07:08:10 -0400

    Started lexer, and add .gitignore file

--- lexer.py
+++ lexer.py
@@ -1,1 +1,12 @@
 # Lexical Analisys
+
+import ply.lex as lex
+from ply.lex import TOKEN
+
+class CoolLexer(object):
+    
+    def __init__(self):
+        self.lexer = None
+        self.tokens = {}
+        self.reserved = {}
+        self.last_token = None
