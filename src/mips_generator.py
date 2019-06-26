049f99d56d75e6bb62878106bed473f0b834380c
¨jr638091¨ <jr638091@gmail.com>
2019-06-26 04:31:47 -0400

    fixed case

--- src/mips_generator.py
+++ src/mips_generator.py
@@ -524,7 +524,7 @@ class MIPS:
         self.mips_code.append(f"addu $sp, $sp, 4")
         self.mips_code.append("lw $ra, 4($sp)")
         self.mips_code.append("addu $sp, $sp, 4")
-        self.mips_code.append("beq $a0, $t0, {}".format(node.action_tag))
+        self.mips_code.append("beqz $a0, {}".format(node.action_tag))
 
         i = self.vars.index(node.id)
         self.mips_code.append(f"sw $t0, -{i*4}($fp)")
